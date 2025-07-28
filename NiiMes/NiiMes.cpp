ec754ffa0ed2779c839e9dc78d3aaa08cad659e0
tamoshka <127463024+tamoshka@users.noreply.github.com>
2025-07-28 13:21:59 +0700

    Добавлены указатели (для Element), добавлена реакция системы на нажатие вторых чекбоксов.

--- NiiMes/NiiMes.cpp
+++ NiiMes/NiiMes.cpp
@@ -46,15 +46,15 @@ void NiiMes::AddValues()
     r1[L] = r1L;
     elementDictionary l1;
     l1[L] = l1L;
-    R1 = Element(r1, 0, "R1");
-    L1 = Element(l1, 1, "L1");
+    R1 = new Element(r1, 0, "R1");
+    L1 = new Element(l1, 1, "L1");
     _elements.append(R1);
     _elements.append(L1);
     ui->ParametersTable->insertRow(1);
-    QTableWidgetItem* name = new QTableWidgetItem(QString::fromStdString(R1.GetName()) + "." + "R");
+    QTableWidgetItem* name = new QTableWidgetItem(QString::fromStdString(R1->GetName()) + "." + "R");
     ui->ParametersTable->setItem(1, 0, name);
-    ui->ParametersTable->setItem(1, 1, new QTableWidgetItem(QString::number(R1.GetParameters(R)->GetValue()) + " Ohm"));
-    checkBoxFirst = new QCheckBox();
+    ui->ParametersTable->setItem(1, 1, new QTableWidgetItem(QString::number(R1->GetParameters(R)->GetValue()) + " Ohm"));
+    QCheckBox* checkBoxFirst = new QCheckBox();
     checkBoxFirst->setChecked(false);
     ui->ParametersTable->setCellWidget(1, 2, checkBoxFirst);
     QCheckBox* checkBoxSecond = new QCheckBox();
@@ -64,36 +64,36 @@ void NiiMes::AddValues()
     ui->ParametersTable->setItem(1, 5, new QTableWidgetItem());
     ui->ParametersTable->setItem(1, 6, new QTableWidgetItem());
     ui->ParametersTable->insertRow(2);
-    QTableWidgetItem* name2 = new QTableWidgetItem(QString::fromStdString(R1.GetName()) + "." + "W");
+    QTableWidgetItem* name2 = new QTableWidgetItem(QString::fromStdString(R1->GetName()) + "." + "W");
     ui->ParametersTable->setItem(2, 0, name2);
-    ui->ParametersTable->setItem(2, 1, new QTableWidgetItem(QString::number(R1.GetParameters(W)->GetValue()) + " um"));
-    checkBoxFirst2 = new QCheckBox();
+    ui->ParametersTable->setItem(2, 1, new QTableWidgetItem(QString::number(R1->GetParameters(W)->GetValue()) + " um"));
+    QCheckBox* checkBoxFirst2 = new QCheckBox();
     checkBoxFirst2->setChecked(true);
     ui->ParametersTable->setCellWidget(2, 2, checkBoxFirst2);
     QCheckBox* checkBoxSecond2 = new QCheckBox();
     checkBoxSecond2->setChecked(false);
     ui->ParametersTable->setCellWidget(2, 3, checkBoxSecond2);
-    ui->ParametersTable->setItem(2, 4, new QTableWidgetItem(QString::number(R1.GetParameters(W)->GetMin()) + " um"));
-    ui->ParametersTable->setItem(2, 5, new QTableWidgetItem(QString::number(R1.GetParameters(W)->GetMax()) + " um"));
-    ui->ParametersTable->setItem(2, 6, new QTableWidgetItem(QString::number(R1.GetParameters(W)->GetStep()) + " um"));
+    ui->ParametersTable->setItem(2, 4, new QTableWidgetItem(QString::number(R1->GetParameters(W)->GetMin()) + " um"));
+    ui->ParametersTable->setItem(2, 5, new QTableWidgetItem(QString::number(R1->GetParameters(W)->GetMax()) + " um"));
+    ui->ParametersTable->setItem(2, 6, new QTableWidgetItem(QString::number(R1->GetParameters(W)->GetStep()) + " um"));
     ui->ParametersTable->insertRow(3);
-    QTableWidgetItem* name3 = new QTableWidgetItem(QString::fromStdString(R1.GetName()) + "." + "L");
+    QTableWidgetItem* name3 = new QTableWidgetItem(QString::fromStdString(R1->GetName()) + "." + "L");
     ui->ParametersTable->setItem(3, 0, name3);
-    ui->ParametersTable->setItem(3, 1, new QTableWidgetItem(QString::number(R1.GetParameters(L)->GetValue()) + " um"));
-    checkBoxFirst3 = new QCheckBox();
+    ui->ParametersTable->setItem(3, 1, new QTableWidgetItem(QString::number(R1->GetParameters(L)->GetValue()) + " um"));
+    QCheckBox* checkBoxFirst3 = new QCheckBox();
     checkBoxFirst3->setChecked(true);
     ui->ParametersTable->setCellWidget(3, 2, checkBoxFirst3);
     QCheckBox* checkBoxSecond3 = new QCheckBox();
     checkBoxSecond3->setChecked(false);
     ui->ParametersTable->setCellWidget(3, 3, checkBoxSecond3);
-    ui->ParametersTable->setItem(3, 4, new QTableWidgetItem(QString::number(R1.GetParameters(L)->GetMin()) + " um"));
-    ui->ParametersTable->setItem(3, 5, new QTableWidgetItem(QString::number(R1.GetParameters(L)->GetMax()) + " um"));
-    ui->ParametersTable->setItem(3, 6, new QTableWidgetItem(QString::number(R1.GetParameters(L)->GetStep()) + " um"));
+    ui->ParametersTable->setItem(3, 4, new QTableWidgetItem(QString::number(R1->GetParameters(L)->GetMin()) + " um"));
+    ui->ParametersTable->setItem(3, 5, new QTableWidgetItem(QString::number(R1->GetParameters(L)->GetMax()) + " um"));
+    ui->ParametersTable->setItem(3, 6, new QTableWidgetItem(QString::number(R1->GetParameters(L)->GetStep()) + " um"));
     ui->ParametersTable->insertRow(4);
-    QTableWidgetItem* name4 = new QTableWidgetItem(QString::fromStdString(L1.GetName()) + "." + "L");
+    QTableWidgetItem* name4 = new QTableWidgetItem(QString::fromStdString(L1->GetName()) + "." + "L");
     ui->ParametersTable->setItem(4, 0, name4);
-    ui->ParametersTable->setItem(4, 1, new QTableWidgetItem(QString::number(L1.GetParameters(L)->GetValue()) + " nH"));
-    checkBoxFirst4 = new QCheckBox();
+    ui->ParametersTable->setItem(4, 1, new QTableWidgetItem(QString::number(L1->GetParameters(L)->GetValue()) + " nH"));
+    QCheckBox* checkBoxFirst4 = new QCheckBox();
     checkBoxFirst4->setChecked(false);
     ui->ParametersTable->setCellWidget(4, 2, checkBoxFirst4);
     QCheckBox* checkBoxSecond4 = new QCheckBox();
@@ -125,14 +125,22 @@ void NiiMes::AddValues()
     checkBoxes.append(checkBoxFirst2);
     checkBoxes.append(checkBoxFirst3);
     checkBoxes.append(checkBoxFirst4);
+    tuneCheckBoxes.append(checkBoxSecond);
+    tuneCheckBoxes.append(checkBoxSecond2);
+    tuneCheckBoxes.append(checkBoxSecond3);
+    tuneCheckBoxes.append(checkBoxSecond4);
     connect(checkBoxFirst, &QCheckBox::toggled, this, &NiiMes::Line);
     connect(checkBoxFirst2, &QCheckBox::toggled, this, &NiiMes::Line);
     connect(checkBoxFirst3, &QCheckBox::toggled, this, &NiiMes::Line);
     connect(checkBoxFirst4, &QCheckBox::toggled, this, &NiiMes::Line);
+    connect(checkBoxSecond, &QCheckBox::toggled, this, &NiiMes::Tune);
+    connect(checkBoxSecond2, &QCheckBox::toggled, this, &NiiMes::Tune);
+    connect(checkBoxSecond3, &QCheckBox::toggled, this, &NiiMes::Tune);
+    connect(checkBoxSecond4, &QCheckBox::toggled, this, &NiiMes::Tune);
     connect(ui->ParametersTable, &QTableWidget::currentCellChanged, this, &NiiMes::OnChanged);
 }
 
-void NiiMes::Line(bool checked)
+void NiiMes::Tune(bool checked)
 {
     int i = 0;
     QCheckBox* checkBox = qobject_cast<QCheckBox*>(sender());
@@ -145,14 +153,14 @@ void NiiMes::Line(bool checked)
         }
         i++;
     }
-    Element element;
+    Element* element;
     string str = ui->ParametersTable->item(i, 0)->text().toUtf8().constData();
     size_t pos = str.find('.');
     string name = str.substr(0, pos);
     string mode1 = str.substr(pos + 1);
     for (auto var : _elements)
     {
-        if (var.GetName() == name)
+        if (var->GetName() == name)
         {
             element = var;
         }
@@ -170,15 +178,57 @@ void NiiMes::Line(bool checked)
     {
         curMode = W;
     }
+    element->GetParameters(curMode)->SetTune(checked);
+}
+
+void NiiMes::Line(bool checked)
+{
+    int i = 0;
+    QCheckBox* checkBox = qobject_cast<QCheckBox*>(sender());
+    for (auto var : checkBoxes)
+    {
+        if (checkBoxes[i] == checkBox)
+        {
+            i += 1;
+            break;
+        }
+        i++;
+    }
+    Element* element;
+    string str = ui->ParametersTable->item(i, 0)->text().toUtf8().constData();
+    size_t pos = str.find('.');
+    string name = str.substr(0, pos);
+    string mode1 = str.substr(pos + 1);
+    for (auto var : _elements)
+    {
+        if (var->GetName() == name)
+        {
+            element = var;
+        }
+    }
+    mode curMode;
+    if (mode1 == "R")
+    {
+        curMode = R;
+    }
+    else if (mode1 == "L")
+    {
+        curMode = L;
+    }
+    else if (mode1 == "W")
+    {
+        curMode = W;
+    }
+    element->GetParameters(curMode)->SetOptimization(checked);
     if (checked == true)
     {
         ui->ParametersTable->item(i, 1)->setFlags(ui->ParametersTable->item(i, 1)->flags() | Qt::ItemIsEnabled);
         ui->ParametersTable->item(i, 4)->setFlags(ui->ParametersTable->item(i, 4)->flags() | Qt::ItemIsEnabled);
         ui->ParametersTable->item(i, 5)->setFlags(ui->ParametersTable->item(i, 5)->flags() | Qt::ItemIsEnabled);
         ui->ParametersTable->item(i, 6)->setFlags(ui->ParametersTable->item(i, 6)->flags() | Qt::ItemIsEnabled);
-        ui->ParametersTable->setItem(i, 4, new QTableWidgetItem(QString::number(element.GetParameters(curMode)->GetMin()) + " um"));
-        ui->ParametersTable->setItem(i, 5, new QTableWidgetItem(QString::number(element.GetParameters(curMode)->GetMax()) + " um"));
-        ui->ParametersTable->setItem(i, 6, new QTableWidgetItem(QString::number(element.GetParameters(curMode)->GetStep()) + " um"));
+        ui->ParametersTable->setItem(i, 4, new QTableWidgetItem(QString::number(element->GetParameters(curMode)->GetMin()) + " um"));
+        ui->ParametersTable->setItem(i, 5, new QTableWidgetItem(QString::number(element->GetParameters(curMode)->GetMax()) + " um"));
+        ui->ParametersTable->setItem(i, 6, new QTableWidgetItem(QString::number(element->GetParameters(curMode)->GetStep()) + " um"));
     }
     else
     {
@@ -199,10 +249,12 @@ void NiiMes::OnChanged(int currentRow, int currentCol, int row, int col)
         string str = ui->ParametersTable->item(row, 0)->text().toUtf8().constData();
         size_t pos = str.find('.');
         string name = str.substr(0, pos);
-        string mode = str.substr(pos + 1);
+        string mode1 = str.substr(pos + 1);
+        mode curMode;
+        QString curStr;
         for (auto var : _elements)
         {
-            if (var.GetName() == name)
+            if (var->GetName() == name)
             {
                 try
                 {
@@ -211,134 +263,60 @@ void NiiMes::OnChanged(int currentRow, int currentCol, int row, int col)
                     string strvalue = newstr.substr(0, newpos);
                     QString qstrvalue = QString::fromUtf8(strvalue);
                     int value = qstrvalue.toInt();
-                    if (mode == "R")
+                    if (mode1 == "R")
                     {
-                        if (col == 1)
-                        {
-                            var.GetParameters(R)->SetValue(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetValue()) + " Ohm"));
-                        }
-                        if (col == 4)
-                        {
-                            var.GetParameters(R)->SetMin(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetMin()) + " um"));
-                        }
-                        if (col == 5)
-                        {
-                            var.GetParameters(R)->SetMax(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetMax()) + " um"));
-                        }
-                        if (col == 6)
-                        {
-                            var.GetParameters(R)->SetStep(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetStep()) + " um"));
-                        }
+                        curMode = R;
+                        curStr = " Ohm";
                     }
-                    else if (mode == "L")
+                    else if (mode1 == "W")
                     {
-                        if (col == 1)
-                        {
-                            var.GetParameters(L)->SetValue(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetValue()) + " Ohm"));
-                        }
-                        if (col == 4)
-                        {
-                            var.GetParameters(L)->SetMin(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetMin()) + " um"));
-                        }
-                        if (col == 5)
-                        {
-                            var.GetParameters(L)->SetMax(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetMax()) + " um"));
-                        }
-                        if (col == 6)
-                        {
-                            var.GetParameters(L)->SetStep(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetStep()) + " um"));
-                        }
+                        curMode = W;
+                        curStr = " um";
                     }
-                    else if (mode == "W")
+                    else if (mode1 == "L")
                     {
-                        if (col == 1)
-                        {
-                            var.GetParameters(W)->SetValue(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetValue()) + " Ohm"));
-                        }
-                        if (col == 4)
-                        {
-                            var.GetParameters(W)->SetMin(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetMin()) + " um"));
-                        }
-                        if (col == 5)
-                        {
-                            var.GetParameters(W)->SetMax(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetMax()) + " um"));
-                        }
-                        if (col == 6)
-                        {
-                            var.GetParameters(W)->SetStep(value);
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetStep()) + " um"));
-                        }
+                        curMode = L;
+                        curStr = " nH";
+                    }
+                    if (col == 1)
+                    {
+                        var->GetParameters(curMode)->SetValue(value);
+                        
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetValue()) + curStr));
+                    }
+                    if (col == 4)
+                    {
+                        var->GetParameters(curMode)->SetMin(value);
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetMin()) + " um"));
+                    }
+                    if (col == 5)
+                    {
+                        var->GetParameters(curMode)->SetMax(value);
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetMax()) + " um"));
+                    }
+                    if (col == 6)
+                    {
+                        var->GetParameters(curMode)->SetStep(value);
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetStep()) + " um"));
                     }
                 }
                 catch (const exception&)
                 {
-                    if (mode == "R")
+                    if (col == 1)
                     {
-                        if (col == 1)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetValue()) + " Ohm"));
-                        }
-                        if (col == 4)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetMin()) + " um"));
-                        }
-                        if (col == 5)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetMax()) + " um"));
-                        }
-                        if (col == 6)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(R)->GetStep()) + " um"));
-                        }
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetValue()) + curStr));
                     }
-                    else if (mode == "L")
+                    if (col == 4)
                     {
-                        if (col == 1)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetValue()) + " nH"));
-                        }
-                        if (col == 4)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetMin()) + " um"));
-                        }
-                        if (col == 5)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetMax()) + " um"));
-                        }
-                        if (col == 6)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(L)->GetStep()) + " um"));
-                        }
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetMin()) + " um"));
                     }
-                    else if (mode == "W")
+                    if (col == 5)
                     {
-                        if (col == 1)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetValue()) + " um"));
-                        }
-                        if (col == 4)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetMin()) + " um"));
-                        }
-                        if (col == 5)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetMax()) + " um"));
-                        }
-                        if (col == 6)
-                        {
-                            ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var.GetParameters(W)->GetStep()) + " um"));
-                        }
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetMax()) + " um"));
+                    }
+                    if (col == 6)
+                    {
+                        ui->ParametersTable->setItem(row, col, new QTableWidgetItem(QString::number(var->GetParameters(curMode)->GetStep()) + " um"));
                     }
                 }
             }
--- NiiMes/NiiMes.h
+++ NiiMes/NiiMes.h
@@ -17,16 +17,14 @@ public:
     ~NiiMes();
     void AddValues();
     void Line(bool);
+    void Tune(bool);
     void OnChanged(int, int, int, int);
 private:
     Ui::NiiMesClass *ui;
-    QList<Element> _elements;
+    QList<Element*> _elements;
     QList <QCheckBox*> checkBoxes;
-    QCheckBox* checkBoxFirst;
-    QCheckBox* checkBoxFirst2;
-    QCheckBox* checkBoxFirst3;
-    QCheckBox* checkBoxFirst4;
-    Element R1;
-    Element L1;
+    QList <QCheckBox*> tuneCheckBoxes;
+    Element* R1;
+    Element* L1;
 };
 
